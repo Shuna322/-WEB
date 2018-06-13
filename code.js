c7a3376c084664d2b1380cf01bc78ff874e0b9f4
Shuna322 <Sashka-pokudko@outlook.com>
2018-06-13 22:29:28 +0300

    update

--- code.js
+++ code.js
@@ -1,30 +1,14 @@
 $(document).ready(function(){
   $('#send').click(function(){
-      validateForm();
+    var name = document.getElementById('name');
+    var sname = document.getElementById('sname');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var passwordrepeat = document.getElementById('passwordrepeat');
+    var adress = document.getElementById('adress');
+
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+    var a = name.concat(sname,email,password,passwordrepeat,adress);
+    document.getElementById("messeage").innerHTML = a;
   });
-
-  function validateForm(){
-    $('.reg').validate({ // initialize the plugin
-        rules: {
-            field1: {
-                required: true,
-                minlength: 5
-            },
-            field2: {
-                required: true,
-                minlength: 5
-            }
-            field3: {
-                required: true,
-                email: true
-              }
-        }
-        messages: {
-          field1 : {
-            required : 'Ввведіть ім"я'
-          }
-        }
-    });
-  }
-
-});
