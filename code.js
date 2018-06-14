d3e8cdce2bcb48dcd06fd99132571942a262fee9
Pokydko Alex <Sashk-pokudko@outlook.com>
2018-06-14 09:34:34 +0300

    finished

--- code.js
+++ code.js
@@ -1,14 +1,57 @@
-$(document).ready(function(){
-  $('#send').click(function(){
-    var name = document.getElementById('name');
-    var sname = document.getElementById('sname');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var passwordrepeat = document.getElementById('passwordrepeat');
-    var adress = document.getElementById('adress');
-
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
-    var a = name.concat(sname,email,password,passwordrepeat,adress);
-    document.getElementById("messeage").innerHTML = a;
+$(document).ready(function() {
+    var passwordrepeat = document.getElementById("passwordrepeat").value;
+    $('#email').change(function() {
+    var email = document.getElementById("email").value;
+    if (email != "" && validateEmail(email) ) {
+      document.getElementById("email").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("email").style.border = "2px solid rgb(204, 0, 0)";
+    }
   });
+  $('#name').change(function() {
+    var name = document.getElementById("name").value;
+    if (name != ""){
+      document.getElementById("name").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("name").style.border = "2px solid rgb(204, 0, 0)";
+    }
+  });
+  $('#sname').change(function() {
+    var sname = document.getElementById("sname").value;
+    if (sname != ""){
+      document.getElementById("sname").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("sname").style.border = "2px solid rgb(204, 0, 0)";
+    }
+  });
+  $('#adress').change(function() {
+    var adress = document.getElementById("adress").value;
+    if (adress != ""){
+      document.getElementById("adress").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("adress").style.border = "2px solid rgb(204, 0, 0)";
+    }
+  });
+  $('#password').change(function() {
+    var password = document.getElementById("password").value;
+    if (password.length > 6){
+      document.getElementById("password").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("password").style.border = "2px solid rgb(204, 0, 0)";
+    }
+  });
+  $('#passwordrepeat').change(function() {
+    var password = document.getElementById("password").value;
+    var passwordrepeat = document.getElementById("passwordrepeat").value;
+    if (password == passwordrepeat){
+      document.getElementById("passwordrepeat").style.border = "2px solid rgb(0, 228, 16)";
+    } else {
+      document.getElementById("passwordrepeat").style.border = "2px solid rgb(204, 0, 0)";
+    }
+  });
+});
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
